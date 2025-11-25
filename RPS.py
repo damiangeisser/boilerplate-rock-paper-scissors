77cf7e6e8b1e5cfe3f72fee68a1a4588566169fa
damiangeisser <dageisser@gmail.com>
2025-11-25 18:16:30 -0300

    resolution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,31 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], pattern_counts={}):
+    
+    if not prev_play:
+        prev_play = "R"
 
-def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    prediction = "P"
 
-    return guess
+    if len(opponent_history) > 4:
+ 
+        last_five = "".join(opponent_history[-5:])
+        pattern_counts[last_five] = pattern_counts.get(last_five, 0) + 1
+
+        last_four = opponent_history[-4:]
+        potential_sequences = [
+            "".join(last_four + [m]) for m in ["R", "P", "S"]
+        ]
+
+        sub_counts = {
+            seq: pattern_counts[seq]
+            for seq in potential_sequences
+            if seq in pattern_counts
+        }
+
+        if sub_counts:
+            best_seq = max(sub_counts, key=sub_counts.get)
+            prediction = best_seq[-1]
+
+    beats = {"R": "P", "P": "S", "S": "R"}
+    return beats[prediction]
